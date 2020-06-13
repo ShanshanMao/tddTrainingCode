f0b07053b8ceb7d243691dbf2a9fa133efb789e4
ShanshanMao <ssmao@thoughtworks.com>
2020-06-13 08:32:18 +0800

    新增第一个tasking的测试代码：储物柜有10个并且未满，存包成功返回小票。

--- TDDLockerHomework/src/test/java/com/LockertTest/LockerTest.java
+++ TDDLockerHomework/src/test/java/com/LockertTest/LockerTest.java
@@ -1,4 +1,21 @@
 package com.LockertTest;
+/**
+ * created by ssmao on 20200613
+ */
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LockerTest {
+
+    @Test
+    public void should_open_a_locker_when_save_a_package_given_loacker_is_10_and_have_a_empty(){
+        Locker locker = new Locker(10);
+        Locker.setUsedCount(9);
+
+        String ticket = locker.save();
+        assertThat(ticket,is("010"));
+    }
 }
