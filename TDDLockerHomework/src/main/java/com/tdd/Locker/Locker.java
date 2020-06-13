15d3ce379d9420bf06ab0a8845ef8cc8d0a21c71
ShanshanMao <ssmao@thoughtworks.com>
2020-06-13 15:33:18 +0800

    增加tasking（拿着有效小票取包成功）的测试和刚刚让测试通过的方法。

--- TDDLockerHomework/src/main/java/com/tdd/Locker/Locker.java
+++ TDDLockerHomework/src/main/java/com/tdd/Locker/Locker.java
@@ -1,20 +1,24 @@
 package com.tdd.Locker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * created by ssmao on 20200613
  */
 public class Locker {
     private int LockerUsedCount;
     private int LockerCount;
+    private List<String> tickets;
 
     public Locker(int LockerCount) {
         this.LockerCount = LockerCount;
+        this.tickets = new ArrayList<String>();
     }
 
     public void setUsedCount(int LockerUsedCount) {
         this.LockerUsedCount = LockerUsedCount;
     }
-
 
     public String save()  {
         if (LockerUsedCount >= LockerCount){
@@ -26,6 +30,12 @@ public class Locker {
 
         return ticket;
         }
+
+    public boolean get(String ticket){
+
+        return true;
+
+    }
 
     }
 
--- TDDLockerHomework/src/test/java/com/LockertTest/LockerTest.java
+++ TDDLockerHomework/src/test/java/com/LockertTest/LockerTest.java
@@ -6,18 +6,28 @@ package com.LockertTest;
 import com.tdd.Locker.Locker;
 import com.tdd.Locker.SavePackageFailException;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LockerTest {
 
+    private Locker locker;
+
+    @Before
+    public void setUp() throws Exception {
+        locker = new Locker(10);
+    }
+
+
     @Test
     public void should_open_a_locker_when_save_a_package_given_locker_is_10_and_have_a_empty(){
 
-        Locker locker = new Locker(10);
+
         locker.setUsedCount(9);
 
         String ticket = locker.save();
@@ -36,6 +46,14 @@ public class LockerTest {
             assertThat(e, is(instanceOf(SavePackageFailException.class)));
             assertThat(e.getMessage(), is("Lockers are fulled,save package failure!"));
         }
+    }
 
+    @Test
+    public void should_open_a_locker_when_pick_a_package_given_ticket_available(){
+        locker.setUsedCount(9);
+        String usedTicket = locker.save();
+
+        boolean success = locker.get(usedTicket);
+        assertTrue(success);
     }
 }
