aa4b3932f0774ecebecbce82ba8a9716cc94c6da
liuyi.hui <liuyi.hui@rea-group.com>
2020-06-20 23:38:39 +0800

    Refactor

--- TDDLockerHomework/src/main/java/com/tdd/Locker/exception/InvalidTicketException.java
+++
@@ -1,8 +0,0 @@
-package com.tdd.Locker.exception;
-
-public class InvalidTicketException extends RuntimeException {
-
-    public InvalidTicketException() {
-        super();
-    }
-}
---
+++ src/main/java/com/locker/Bag.java
@@ -0,0 +1,4 @@
+package com.locker;
+
+public class Bag {
+}
