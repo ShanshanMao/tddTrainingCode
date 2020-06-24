642c612d70e70f5ce8f63245d7fd9726cf0f3a39
hui61 <huiliuyi@gmail.com>
2020-06-24 16:12:20 +0800

    Test 14: should return when pickUp bag with valid ticket and LockerRobotManager have 1 locker and 1 robot

--- src/test/java/com/locker/LockerRobotManagerTest.java
+++ src/test/java/com/locker/LockerRobotManagerTest.java
@@ -12,7 +12,7 @@ public class LockerRobotManagerTest {
     @Test
     public void should_return_a_ticket_and_store_in_1st_locker_when_LockerRobotManager_have_2_locker_with_available_capacity_and_no_robot() {
         Locker firstLocker = new Locker(1);
-        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(firstLocker, new Locker(1)),Collections.emptyList());
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(firstLocker, new Locker(1)), Collections.emptyList());
         Bag myBag = new Bag();
         Ticket ticket = lockerRobotManager.store(myBag);
         Assert.assertNotNull(ticket);
@@ -20,57 +20,57 @@ public class LockerRobotManagerTest {
     }
 
     @Test
-    public void should_return_a_ticket_and_store_in_2nd_locker_when_LockerRobotManager_have_1_full_locker_1_available_capacity_locker_and_no_robot(){
+    public void should_return_a_ticket_and_store_in_2nd_locker_when_LockerRobotManager_have_1_full_locker_1_available_capacity_locker_and_no_robot() {
         Locker firstLocker = new Locker(1);
         firstLocker.store(new Bag());
         Locker secondLocker = new Locker(1);
-        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(firstLocker, secondLocker),Collections.emptyList());
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(firstLocker, secondLocker), Collections.emptyList());
         Bag myBag = new Bag();
         Ticket ticket = lockerRobotManager.store(myBag);
         Assert.assertNotNull(ticket);
-        Assert.assertSame(myBag,secondLocker.pickUp(ticket));
+        Assert.assertSame(myBag, secondLocker.pickUp(ticket));
     }
 
     @Test(expected = NoRoomException.class)
-    public void should_return_a_NoRoomException_when_LockerRobotManager_have_2_full_lockers_and_no_robot(){
+    public void should_return_a_NoRoomException_when_LockerRobotManager_have_2_full_lockers_and_no_robot() {
         Locker firstLocker = new Locker(1);
         firstLocker.store(new Bag());
         Locker secondLocker = new Locker(1);
         secondLocker.store(new Bag());
-        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(firstLocker, secondLocker),Collections.emptyList());
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(firstLocker, secondLocker), Collections.emptyList());
         lockerRobotManager.store(new Bag());
     }
 
     @Test
     public void should_return_a_ticket_and_store_in_1st_robot_when_LockerRobotManager_have_2_robot_with_available_capacity_but_no_locker() {
-        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Arrays.asList(new Locker(1),new Locker(1)));
-        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(new Locker(1),new Locker(1)));
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Arrays.asList(new Locker(1), new Locker(1)));
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(new Locker(1), new Locker(1)));
         LockerRobotManager lockerRobotManager = new LockerRobotManager(null, Arrays.asList(primaryLockerRobot, smartLockerRobot));
         Bag myBag = new Bag();
         Ticket ticket = lockerRobotManager.store(myBag);
         Assert.assertNotNull(ticket);
-        Assert.assertSame(myBag,primaryLockerRobot.pickUp(ticket));
+        Assert.assertSame(myBag, primaryLockerRobot.pickUp(ticket));
     }
 
     @Test
-    public void should_return_a_ticket_and_store_in_2nd_robot_when_LockerRobotManager_have_1st_full_robot_and_2nd_robot_with_available_capacity_but_no_locker(){
-        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Arrays.asList(new Locker(1),new Locker(1)));
+    public void should_return_a_ticket_and_store_in_2nd_robot_when_LockerRobotManager_have_1st_full_robot_and_2nd_robot_with_available_capacity_but_no_locker() {
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Arrays.asList(new Locker(1), new Locker(1)));
         primaryLockerRobot.store(new Bag());
         primaryLockerRobot.store(new Bag());
-        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(new Locker(1),new Locker(1)));
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(new Locker(1), new Locker(1)));
         LockerRobotManager lockerRobotManager = new LockerRobotManager(null, Arrays.asList(primaryLockerRobot, smartLockerRobot));
         Bag myBag = new Bag();
         Ticket ticket = lockerRobotManager.store(myBag);
         Assert.assertNotNull(ticket);
-        Assert.assertSame(myBag,smartLockerRobot.pickUp(ticket));
+        Assert.assertSame(myBag, smartLockerRobot.pickUp(ticket));
     }
 
     @Test(expected = NoRoomException.class)
-    public void should_return_a_NoRoomException_when_LockerRobotManager_have_2_full_robot_and_no_locker(){
-        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Arrays.asList(new Locker(1),new Locker(1)));
+    public void should_return_a_NoRoomException_when_LockerRobotManager_have_2_full_robot_and_no_locker() {
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Arrays.asList(new Locker(1), new Locker(1)));
         primaryLockerRobot.store(new Bag());
         primaryLockerRobot.store(new Bag());
-        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(new Locker(1),new Locker(1)));
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(new Locker(1), new Locker(1)));
         smartLockerRobot.store(new Bag());
         smartLockerRobot.store(new Bag());
         LockerRobotManager lockerRobotManager = new LockerRobotManager(Collections.emptyList(), Arrays.asList(primaryLockerRobot, smartLockerRobot));
@@ -79,18 +79,18 @@ public class LockerRobotManagerTest {
     }
 
     @Test
-    public void should_return_a_ticket_when_LockerRobotManager_have_1_robot_and_1_locker_both_have_available_capacity(){
+    public void should_return_a_ticket_when_LockerRobotManager_have_1_robot_and_1_locker_both_have_available_capacity() {
         PrimaryLockerRobot lockerRobot = new PrimaryLockerRobot(Arrays.asList(new Locker(1), new Locker(1)));
         Locker locker = new Locker(1);
         LockerRobotManager lockerRobotManager = new LockerRobotManager(Collections.singletonList(locker), Collections.singletonList(lockerRobot));
         Bag myBag = new Bag();
         Ticket ticket = lockerRobotManager.store(myBag);
         Assert.assertNotNull(ticket);
-        Assert.assertSame(myBag,lockerRobot.pickUp(ticket));
+        Assert.assertSame(myBag, lockerRobot.pickUp(ticket));
     }
 
     @Test
-    public void should_return_a_ticket_and_store_in_locker_when_LockerRobotManager_have_1_full_robot_and_1_locker_have_available_capacity(){
+    public void should_return_a_ticket_and_store_in_locker_when_LockerRobotManager_have_1_full_robot_and_1_locker_have_available_capacity() {
         PrimaryLockerRobot lockerRobot = new PrimaryLockerRobot(Collections.singletonList(new Locker(1)));
         lockerRobot.store(new Bag());
         Locker locker = new Locker(1);
@@ -98,11 +98,11 @@ public class LockerRobotManagerTest {
         Bag myBag = new Bag();
         Ticket ticket = lockerRobotManager.store(myBag);
         Assert.assertNotNull(ticket);
-        Assert.assertSame(myBag,locker.pickUp(ticket));
+        Assert.assertSame(myBag, locker.pickUp(ticket));
     }
 
     @Test(expected = NoRoomException.class)
-    public void should_return_NoRoomException_when_LockerRobotManager_have_1_full_robot_and_1_full_locker(){
+    public void should_return_NoRoomException_when_LockerRobotManager_have_1_full_robot_and_1_full_locker() {
         PrimaryLockerRobot lockerRobot = new PrimaryLockerRobot(Collections.singletonList(new Locker(1)));
         lockerRobot.store(new Bag());
         Locker locker = new Locker(1);
@@ -113,7 +113,7 @@ public class LockerRobotManagerTest {
     }
 
     @Test
-    public void should_return_bag_when_pickUp_bag_with_valid_ticket_and_LockerRobotManager_have_2_lockers_but_no_robot(){
+    public void should_return_bag_when_pickUp_bag_with_valid_ticket_and_LockerRobotManager_have_2_lockers_but_no_robot() {
         Locker firstLocker = new Locker(1);
         Locker secondLocker = new Locker(1);
         LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(firstLocker, secondLocker), Collections.emptyList());
@@ -123,7 +123,7 @@ public class LockerRobotManagerTest {
     }
 
     @Test(expected = InvalidTicketException.class)
-    public void should_return_InvalidTicketException_when_pickUp_bag_with_inValid_ticket_and_LockerRobotManager_have_2_lockers_but_no_robot(){
+    public void should_return_InvalidTicketException_when_pickUp_bag_with_inValid_ticket_and_LockerRobotManager_have_2_lockers_but_no_robot() {
         Locker firstLocker = new Locker(1);
         Locker secondLocker = new Locker(1);
         LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(firstLocker, secondLocker), Collections.emptyList());
@@ -133,7 +133,7 @@ public class LockerRobotManagerTest {
     }
 
     @Test
-    public void should_return_bag_when_pickUp_bag_with_valid_ticket_and_LockerRobotManager_have_no_locker_but_2_robot_with_available_capacity(){
+    public void should_return_bag_when_pickUp_bag_with_valid_ticket_and_LockerRobotManager_have_no_locker_but_2_robot_with_available_capacity() {
         PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Collections.singletonList(new Locker(1)));
         SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Collections.singletonList(new Locker(1)));
         LockerRobotManager lockerRobotManager = new LockerRobotManager(Collections.emptyList(), Arrays.asList(primaryLockerRobot, smartLockerRobot));
@@ -143,12 +143,22 @@ public class LockerRobotManagerTest {
     }
 
     @Test(expected = InvalidTicketException.class)
-    public void should_return_InvalidTicketException_when_pickUp_bag_with_inValid_ticket_and_LockerRobotManager_have_no_locker_but_2_robot_with_available_capacity(){
+    public void should_return_InvalidTicketException_when_pickUp_bag_with_inValid_ticket_and_LockerRobotManager_have_no_locker_but_2_robot_with_available_capacity() {
         PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Collections.singletonList(new Locker(1)));
         SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Collections.singletonList(new Locker(1)));
         LockerRobotManager lockerRobotManager = new LockerRobotManager(Collections.emptyList(), Arrays.asList(primaryLockerRobot, smartLockerRobot));
         Bag myBag = new Bag();
         lockerRobotManager.store(myBag);
         lockerRobotManager.pickUp(new Ticket());
+    }
+
+    @Test
+    public void should_return_when_pickUp_bag_with_valid_ticket_and_LockerRobotManager_have_1_locker_and_1_robot() {
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Collections.singletonList(new Locker(1)));
+        Locker locker = new Locker(1);
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Collections.singletonList(locker), Collections.singletonList(primaryLockerRobot));
+        Bag myBag = new Bag();
+        Ticket ticket = lockerRobotManager.store(myBag);
+        Assert.assertSame(myBag, lockerRobotManager.pickUp(ticket));
     }
 }
