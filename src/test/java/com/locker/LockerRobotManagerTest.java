6253df967ebb0d30d35b44fd739fb2264992295e
hui61 <huiliuyi@gmail.com>
2020-06-24 14:35:06 +0800

    Test 2: should return a ticket and store in 2nd locker when LockerRobotManager have 1 full locker 1 available capacity locker and no robot

--- src/test/java/com/locker/LockerRobotManagerTest.java
+++ src/test/java/com/locker/LockerRobotManagerTest.java
@@ -1,9 +1,11 @@
 package com.locker;
 
+import com.sun.corba.se.impl.orbutil.LogKeywords;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.concurrent.locks.Lock;
 
 public class LockerRobotManagerTest {
     @Test
@@ -14,5 +16,17 @@ public class LockerRobotManagerTest {
         Ticket ticket = lockerRobotManager.store(myBag);
         Assert.assertNotNull(ticket);
         Assert.assertSame(myBag, firstLocker.pickUp(ticket));
+    }
+
+    @Test
+    public void should_return_a_ticket_and_store_in_2nd_locker_when_LockerRobotManager_have_1_full_locker_1_available_capacity_locker_and_no_robot(){
+        Locker firstLocker = new Locker(1);
+        firstLocker.store(new Bag());
+        Locker secondLocker = new Locker(1);
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(firstLocker, secondLocker));
+        Bag myBag = new Bag();
+        Ticket ticket = lockerRobotManager.store(myBag);
+        Assert.assertNotNull(ticket);
+        Assert.assertSame(myBag,secondLocker.pickUp(ticket));
     }
 }
