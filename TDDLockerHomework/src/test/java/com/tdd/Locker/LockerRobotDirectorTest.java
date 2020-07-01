1d18655d6c072782a9cc0f5be201630a2bf00dc6
ShanshanMao <ssmao@thoughtworks.com>
2020-07-01 14:36:53 +0800

    增加task2：LockerRobotDirector管理一个locker和一个PrimaryLockerRobot，PrimaryLockerRobot管理一个locker。查看报表成功

--- TDDLockerHomework/src/test/java/com/tdd/Locker/LockerRobotDirectorTest.java
+++ TDDLockerHomework/src/test/java/com/tdd/Locker/LockerRobotDirectorTest.java
@@ -3,6 +3,8 @@ package com.tdd.Locker;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.List;
+
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
@@ -26,5 +28,26 @@ public class LockerRobotDirectorTest {
                 "\tL 4 4\n";
         assertEquals(expectReport,report);
     }
+
+    @Test
+    public void should_return_report_when_LockerRobotDirector_check_report_given_LockerRobotManager_manage_1_locker_and_1_robot(){
+        Locker firstLocker = new Locker(5);
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(singletonList(new Locker(10)));
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(singletonList(firstLocker),singletonList(primaryLockerRobot));
+        LockerRobotDirector lockerRobotDirector = new LockerRobotDirector(singletonList(lockerRobotManager));
+
+        Bag myBag = new Bag();
+        firstLocker.store(myBag);
+        myBag = new Bag();
+        lockerRobotManager.store(myBag);
+
+        String report = lockerRobotDirector.checkReport();
+        String expectReport = "M 13 15\n"+
+                "\tL 4 5\n"+
+                "\tR 9 10\n"+
+                "\t\tL 9 10\n";
+        assertEquals(expectReport,report);
+    }
+
 
 }
